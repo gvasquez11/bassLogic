ca9eb55008edd2ab2dfcdf0e50fb401f5cf2ecb5
gvasquez11 <vasquez.greg16@gmail.com>
2023-02-25 22:05:33 -0800

    added toggle password for signup page

---
+++ js/signup.js
@@ -0,0 +1,19 @@
+document.getElementById('togglePassword').onclick = showPassword
+
+function showPassword(){
+    console.log('entering function')
+    this.classList.remove('fa-eye');
+
+    if(password.type === 'password'){
+        password.type = 'text'
+        this.classList.toggle('fa-eye');
+        console.log('open eye')
+    }
+    else
+    {
+        password.type = 'password'
+    
+    }
+
+}
+
