42312224267444651a2bc137f46ed270fa410254
gvasquez11 <vasquez.greg16@gmail.com>
2023-03-01 23:08:35 -0800

    adding landing page for student dashboard once logging in

--- js/login.js
+++ js/login.js
@@ -59,6 +59,13 @@ function showPassword(){
                 })
                 alert('Welcome Back!')
 
+                        // User is signed in, see docs for a list of available properties
+                        // https://firebase.google.com/docs/reference/js/firebase.User
+                        location.href = "studentPage.html"
+
+              
+
+
             })
             .catch((error) => {
                 const errorCode = error.code;
